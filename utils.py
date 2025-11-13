e7a3b419ccbfd7461a73883af0c6c2afad3f70e2
Zain Mushtaq <zainmushtaq5439@gmail.com>
2025-11-13 12:44:59 +0500

    Improve search_jobs function to handle NaN values

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 def search_jobs(df, query):
+    """Filter jobs by search query."""
     if not query:
         return df
     query = query.lower()
-    return df[df['title'].str.lower().str.contains(query) | df['company'].str.lower().str.contains(query)]
+    return df[
+        df["title"].str.lower().str.contains(query, na=False)
+        | df["company"].str.lower().str.contains(query, na=False)
+    ]
