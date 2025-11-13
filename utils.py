2478b4b56c374685386029994e9cf11dd7df020e
Zain Mushtaq <zainmushtaq5439@gmail.com>
2025-11-13 12:28:28 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,7 @@
 import pandas as pd
-from fpdf import FPDF
 
-def save_to_csv(df, filename="jobs.csv"):
-    df.to_csv(filename, index=False)
-    return filename
-
-def save_to_pdf(df, filename="jobs.pdf"):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Job Listings", ln=True, align="C")
-
-    for _, row in df.iterrows():
-        pdf.multi_cell(0, 8, f"{row['title']} - {row['company']}\n{row['link']}\n", border=0)
-        pdf.ln(2)
-    pdf.output(filename)
-    return filename
+def search_jobs(df, query):
+    if not query:
+        return df
+    query = query.lower()
+    return df[df['title'].str.lower().str.contains(query) | df['company'].str.lower().str.contains(query)]
